ba43d770b3155910bfe0b8cd69c01ebc3c0ebc91
Natalya Minyaylo <natalya.minyaylo@arcadia.spb.ru>
2019-12-18 19:40:34 +0300

    App for solve geometric tasks is added

---
+++ Solution5/geometric_tasks_app/Program.cs
@@ -0,0 +1,42 @@
+﻿using Classes;
+using System;
+
+namespace geometric_tasks_app
+{
+    class Program
+    {
+        static int InsertIntegerValue()
+        {
+            var result = false;
+            int resultConverted = 0;
+            while (!result)
+            {
+                var userInput = Console.ReadLine();
+                result = int.TryParse(userInput, out resultConverted);
+                if (!result)
+                {
+                    Console.WriteLine("You have entered not an integer! Please insert integer value:");
+                }
+            }
+            return resultConverted;
+        }
+
+        static void Main()
+        {
+            Console.WriteLine("Write the length of the first side of the rectangle:");
+            int side1 = InsertIntegerValue();
+
+            Console.WriteLine("Write the length of the second side of the rectangle:");
+            int side2 = InsertIntegerValue();
+
+            Rectangle userRectangle = new Rectangle(side1, side2);
+
+            int rectanglePerimeter = userRectangle.PerimeterCalculator();
+            int rectangleArea = userRectangle.AreaCalculator();
+
+            Console.WriteLine($"The area of the rectangle is {rectangleArea}, the perimeter of the rectangle is {rectanglePerimeter}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Solution5/geometric_tasks_app/Rectangle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Classes
+{
+    class Rectangle
+    {
+        private int side1;
+        private int side2;
+
+        public Rectangle(int a, int b) { side1 = a; side2 = b; }
+
+        public int AreaCalculator()
+        {
+            int rectangleArea = side1 * side2;
+            return rectangleArea;
+        }
+
+        public int PerimeterCalculator()
+        {
+            int rectanglePerimeter = 2 * (side1 + side2);
+            return rectanglePerimeter;
+        }
+    }
+}
