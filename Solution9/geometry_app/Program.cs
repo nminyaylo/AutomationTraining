38b9535bc7a4f37f7150f81024034735bc7d96ed
Natalya Minyaylo <natalya.minyaylo@arcadia.spb.ru>
2020-02-14 00:16:43 +0300

    Home work of lesson 9: geometry callculation app have added

---
+++ Solution9/geometry_app/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+
+namespace geometry_app
+{
+    class Program
+    {
+        static string InsertedFigure()
+        {
+            string figureType = "";
+            var match = false;
+            while (!match)
+            {
+                string userInput = Console.ReadLine();
+
+                switch (userInput)
+                {
+                    case "R":
+                        Console.WriteLine("You have chosen a rectangle");
+                        figureType = "R";
+                        match = true;
+                        break;
+                    case "S":
+                        Console.WriteLine("You have chosen a square");
+                        figureType = "S";
+                        match = true;
+                        break;
+                    case "T":
+                        Console.WriteLine("You have chosen a triangle");
+                        figureType = "T";
+                        match = true;
+                        break;
+                    default:
+                        Console.WriteLine("You have entered not an appropriate value! Please enter one of the letters: R (for rectangle), S (for square), T (for triangle)");
+                        break;
+                }
+            }
+            return figureType;
+        }
+
+        static float InsertIntegerValue()
+        {
+            var result = false;
+            float resultConverted = 0;
+            while (!result)
+            {
+                var userInput = Console.ReadLine();
+                result = float.TryParse(userInput, out resultConverted);
+                if (!result)
+                {
+                    Console.WriteLine("You have entered not an numerical value! Please insert numerical value:");
+                }
+            }
+            return resultConverted;
+        }
+
+        static void Main()
+        {
+            Console.WriteLine("Enter type of figure using following letters: R (for rectangle), S (for square), T (for triangle)");
+            string insertedFigure = InsertedFigure();
+            Figure figure = null;
+
+            switch (insertedFigure)
+            {
+                case "R":
+                    Console.WriteLine("Enter the length of the first side of the rectangle");
+                    float rectangeSideOne = InsertIntegerValue();
+
+                    Console.WriteLine("Enter the length of the second side of the rectangle");
+                    float rectangeSideTwo = InsertIntegerValue();
+
+                    figure = new Rectangle(rectangeSideOne, rectangeSideTwo);
+
+                    break;
+
+                case "S":
+                    Console.WriteLine("Enter the length of the sides of the square");
+                    float sideSquare = InsertIntegerValue();
+
+                    figure = new Square(sideSquare);
+
+                    break;
+
+                case "T":
+                    Console.WriteLine("Enter the length of the first side of the triangle");
+                    float triangleSideOne = InsertIntegerValue();
+
+                    Console.WriteLine("Enter the length of the second side of the triangle");
+                    float triangleSideTwo = InsertIntegerValue();
+
+                    Console.WriteLine("Enter the length of the third side of the triangle");
+                    float triangleSideThree = InsertIntegerValue();
+
+                    figure = new Triangle(triangleSideOne, triangleSideTwo, triangleSideThree);
+
+                    break;
+            }
+
+            float area = figure.Area();
+            Console.WriteLine($"Area: {area}");
+
+            float perimeter = figure.Perimeter();
+            Console.WriteLine($"Perimeter: {perimeter}");
+
+            float radius = figure.Radius();
+            Console.WriteLine($"Circle radius: {radius}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Solution9/geometry_app/Rectangle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace geometry_app
+{
+    class Rectangle : Figure
+    {
+        private float rectangeSideOne;
+        private float rectangeSideTwo;
+
+        public Rectangle(float rectangeSideOne, float rectangeSideTwo)
+        {
+            this.rectangeSideOne = rectangeSideOne;
+            this.rectangeSideTwo = rectangeSideTwo;
+        }
+
+        public override float Area()
+        {
+            return rectangeSideOne * rectangeSideTwo;
+        }
+
+        public override float Perimeter()
+        {
+            return (rectangeSideOne + rectangeSideTwo) * 2;
+        }
+
+        public override float Radius()
+        {
+            return (float)Math.Sqrt(Math.Pow (rectangeSideOne, 2) + Math.Pow(rectangeSideTwo, 2)) / 2;
+        }
+    }
+}
