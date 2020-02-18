810fbd4de0514ef672eb3612c42f8d515bb87cdc
Natalya Minyaylo <natalya.minyaylo@arcadia.spb.ru>
2020-02-18 20:36:49 +0300

    'Single' exercise has been added

---
+++ Solution9/Singleton/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Single
+{
+    class Single
+    {
+        public static Single instance = new Single();
+        private Single() { }
+    }
+
+
+    class Program
+    {
+        static void Main()
+        {
+            var instanceOne = Single.instance;
+            var instanceOneHash = instanceOne.GetHashCode();
+
+            var instanceTwo = Single.instance;
+            var instanceTwoHash = instanceTwo.GetHashCode();
+
+            var equalHash = instanceOneHash == instanceTwoHash;
+
+            Console.WriteLine($"Fifrs hash {instanceOneHash} and second hash {instanceTwoHash}. They are equal - {equalHash}");
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
